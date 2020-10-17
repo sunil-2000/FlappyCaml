ff7980514899ca2e69b59999e295668820953da9
Sunil <svs57@cornell.edu>
2020-10-17 16:01:43 -0400

    game file skeleton

---
+++ game.mli
@@ -0,0 +1,15 @@
+(** Game module contains functions that the game relies on. For example, 
+    collisions and movement are implemented in this module. *)
+
+
+type t 
+
+val jump: t -> unit 
+
+val fall: t -> unit  
+
+val run: t -> unit 
+
+val fly: t -> unit 
+
+val collide: t -> t 
