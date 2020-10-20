3f7bae1254a9312814e53c71ad93ae8b711c5864
Rishik Zaparde <rz293@cornell.edu>
2020-10-19 23:09:49 -0400

    working graphics with ball going up

--- _build/gui.ml
+++ _build/gui.ml
@@ -1,1 +1,31 @@
 open Graphics
+
+let x = 300
+
+let y = 200
+
+let draw = 
+  open_graph " 600x400"; 
+
+  plot 5 5;
+
+  set_window_title "Flappy Caml"; 
+
+  set_color (rgb 255 0 0);
+
+  for y = 200 to 500 do 
+
+    set_color (rgb 255 0 0);
+
+    fill_circle x (y + 50) 40;
+
+    Unix.sleepf 0.01;
+
+    set_color (rgb 255 255 255);
+
+    fill_circle x (y + 50) 40;
+
+  done
+
+
+
